f9fa2b73c0ac8e8c67eab5bf8cb16a81dd1649d2
Ahasanul Hoque <ahoque24@gmail.com>
2021-09-08 22:39:41 -0400

    Replaced shipStatus in Ship object with isSunk() and added hit() to Ship object. Wrote passing tests for Ship

--- src/Ship.js
+++ src/Ship.js
@@ -1,26 +1,41 @@
 const Ship = (length) => {
   /*
-     shipHealth will be array with index representing one space on the ship
+     shipHealth will be array with each element representing one space on the ship
      1: space has not been hit
      0: space has been hit
   */
-  let shipHealth = [];
+  const shipHealth = [];
 
-  /*
-    shipStatus is boolean to determine whether ship has sunk
-    true: ship has not sunk
-    false: ship has sunk
-  */
-  let shipStatus = true;
-
-  // When a Ship is created, _createShip runs to initialize the shipHealth array
-  const _createShip = (() => {
+  const _createShip = (function createShipOfProvidedLength() {
+    // When a Ship is created, _createShip runs to initialize the shipHealth array
     for (let i = 0; i < length; i++) {
       shipHealth.push(1);
     }
   })();
 
-  return { length, shipHealth, shipStatus };
+  const hit = function hitShip(target) {
+    /* 
+      hit() takes in target space that is being attacked.
+      If it has not been attacked yet, it it set to 0
+    */
+    if (shipHealth[target] === 1) {
+      shipHealth[target] = 0;
+    }
+  };
+
+  const isSunk = function testIfShipSunk() {
+    /*
+      isSunk() checks every element of array for whether they have been hit
+      If they have all been hit, return true
+      If not, return false
+    */
+    if (shipHealth.every((e) => e === 0)) {
+      return true;
+    }
+    return false;
+  };
+
+  return { length, shipHealth, hit, isSunk };
 };
 
 export { Ship };
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -2,9 +2,49 @@ import { Ship } from '../Ship.js';
 
 test('1: Create battleship', () => {
   const testShip = Ship(5);
-  expect(testShip).toEqual({
+  expect(testShip).toMatchObject({
     length: 5,
     shipHealth: [1, 1, 1, 1, 1],
-    shipStatus: true,
   });
+});
+
+test('2: Ship should not be sunk when first created', () => {
+  const testShip = Ship(5);
+  expect(testShip.isSunk()).toBe(false);
+});
+
+test('3: Test hitting a ship', () => {
+  const testShip = Ship(4);
+  testShip.hit(3);
+  expect(testShip.shipHealth).toStrictEqual([1, 1, 1, 0]);
+});
+
+test('4: Hit ship multiple times', () => {
+  const testShip = Ship(4);
+  testShip.hit(0);
+  testShip.hit(2);
+  expect(testShip.shipHealth).toStrictEqual([0, 1, 0, 1]);
+});
+
+test('5: Hit ship multiple times but it has not been sunk', () => {
+  const testShip = Ship(4);
+  testShip.hit(1);
+  testShip.hit(2);
+  expect(testShip.isSunk()).toBe(false);
+});
+
+test('6: Sink the ship', () => {
+  const testShip = Ship(3);
+  testShip.hit(0);
+  testShip.hit(1);
+  testShip.hit(2);
+  expect(testShip.isSunk()).toBe(true);
+});
+
+test('7: See sunken ship', () => {
+  const testShip = Ship(3);
+  testShip.hit(0);
+  testShip.hit(1);
+  testShip.hit(2);
+  expect(testShip).toMatchObject({ length: 3, shipHealth: [0, 0, 0] });
 });
