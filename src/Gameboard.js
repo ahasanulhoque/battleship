4b113098a5df83b1c4a94c2e13082434bbb08f4c
Ahasanul Hoque <ahoque24@gmail.com>
2021-09-12 22:04:24 -0400

    Added comments for private _createBoard function that runs each time a Gameboard is created

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -2,8 +2,12 @@ const Gameboard = () => {
   const board = [];
 
   const _createBoard = (function createEmptyBoard() {
+    /*
+        This function will create an empty gameboard
+        Empty spaces are represented by 0
+    */
     for (let i = 0; i < 10; i++) {
-      const newRow = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+      const newRow = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       board.push(newRow);
     }
   })();
