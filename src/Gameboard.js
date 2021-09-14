ce183bf6a95e4b616d19f33e2b64634b4543b04b
Ahasanul Hoque <ahoque24@gmail.com>
2021-09-13 22:38:14 -0400

    Created placeShip() function in Gameboard factory function and wrote simple tests to make sure a new ship can be placed

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -11,7 +11,42 @@ const Gameboard = () => {
     }
   })();
 
-  return { board };
+  const placeShip = function placeShipAtSpecifiedCoordinates(
+    ship,
+    row,
+    column,
+    orientation
+  ) {
+    // Ship is a ship object created elsewhere
+    // row - start position of ship in first board array element
+    // column - start position of ship in second board array element
+    // orientation - values are "vertical" or "horizontal"
+
+    if (row < 10 && column < 10) {
+      // First verify starting positions are valid
+      if (orientation === 'vertical' && row + ship.length < 10) {
+        /* 
+          If ship is being placed vertically, verify placement is valid
+          Then place ship
+        */
+
+        for (let i = 0; i < ship.length; i++) {
+          board[row + i][column] = ship.shipHealth[i];
+        }
+      } else if (orientation === 'horizontal' && column + ship.length < 10) {
+        /* 
+          If ship is being placed horizontally, verify placement is valid
+          Then place ship
+        */
+
+        for (let i = 0; i < ship.length; i++) {
+          board[row][column + i] = ship.shipHealth[i];
+        }
+      }
+    }
+  };
+
+  return { board, placeShip };
 };
 
 export { Gameboard };
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,4 +1,5 @@
 import { Gameboard } from '../Gameboard.js';
+import { Ship } from '../Ship.js';
 
 test('1: Create gameboard', () => {
   const testBoard = Gameboard();
@@ -6,6 +7,46 @@ test('1: Create gameboard', () => {
     board: [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+});
+
+test('2: Place a ship vertically', () => {
+  const testBoard = Gameboard();
+  const testShip = Ship(3);
+  testBoard.placeShip(testShip, 1, 3, 'vertical');
+  expect(testBoard).toMatchObject({
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+});
+
+test('3: Place a ship horizontally', () => {
+  const testBoard = Gameboard();
+  const testShip = Ship(5);
+  testBoard.placeShip(testShip, 1, 3, 'horizontal');
+  expect(testBoard).toMatchObject({
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
