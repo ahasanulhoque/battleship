353cef545785b177a2515f85f7ddc78f6ff2d596
Ahasanul Hoque <ahoque24@gmail.com>
2021-09-21 21:41:18 -0400

    Added receiveAttack() function to Gameboard object and wrote passing tests to make sure specific ships can be hit, shots can miss, and board can be updated. Added arrays to Gameboard object to keep track of ships and missed shots

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -11,6 +11,11 @@ const Gameboard = () => {
     }
   })();
 
+  // missedShots is array that will keep track of coordinates where miss occurred
+  const missedShots = [];
+
+  const allShips = [];
+
   const placeShip = function placeShipAtSpecifiedCoordinates(
     ship,
     row,
@@ -43,6 +48,10 @@ const Gameboard = () => {
             }
             break;
           }
+
+          if (i === ship.length - 1) {
+            allShips.push(ship);
+          }
         }
       } else if (orientation === 'horizontal' && column + ship.length < 10) {
         /* 
@@ -63,12 +72,44 @@ const Gameboard = () => {
             }
             break;
           }
+
+          if (i === ship.length - 1) {
+            allShips.push(ship);
+          }
         }
       }
     }
   };
 
-  return { board, placeShip };
+  const receiveAttack = function receiveAttackAtSpecifiedCoordinates(
+    row,
+    column
+  ) {
+    // Check if coordinates have value other than 0 and -1
+    if (board[row][column] != 0 && board[row][column] != -1) {
+      // Get ship number to be used as element in allShips
+      const shipNumber = board[row][column].charAt(
+        board[row][column].length - 2
+      );
+
+      // Get position on ship to be passed to ship's hit() function
+      const shipTarget = board[row][column].charAt(
+        board[row][column].length - 1
+      );
+
+      // Hit ship
+      allShips[shipNumber].hit(shipTarget);
+
+      // Change value on board to match ship value
+      board[row][column] = -1;
+    } else if (board[row][column] === 0) {
+      missedShots.push([row, column]);
+    }
+  };
+
+  // function to check if all ships are sunk
+
+  return { board, missedShots, allShips, placeShip, receiveAttack };
 };
 
 export { Gameboard };
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -26,9 +26,9 @@ test('2: Place a ship vertically', () => {
   expect(testBoard).toMatchObject({
     board: [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A00', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A01', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A02', 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -46,7 +46,7 @@ test('3: Place a ship horizontally', () => {
   expect(testBoard).toMatchObject({
     board: [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 'A0', 'A0', 'A0', 'A0', 0, 0],
+      [0, 0, 0, 'A00', 'A01', 'A02', 'A03', 'A04', 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -108,7 +108,7 @@ test("6: Can't place ship if it would intersect with another ship (opposite orie
   expect(testBoard).toMatchObject({
     board: [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 'A0', 'A0', 'A0', 0, 0, 0, 0, 0, 0],
+      [0, 'A00', 'A01', 'A02', 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -134,7 +134,7 @@ test("7: Can't place ship if it would intersect with another ship (same row)", (
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 'A0', 'A0', 'A0', 0],
+      [0, 0, 0, 0, 0, 0, 'A00', 'A01', 'A02', 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -154,11 +154,11 @@ test("8: Can't place ship if it would intersect with another ship (same column)"
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A00', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A01', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A02', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A03', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A04', 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
@@ -174,15 +174,213 @@ test('9: Can place multiple ships if they do not intersect', () => {
   expect(testBoard).toMatchObject({
     board: [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A1', 'A1', 'A1', 'A1', 0, 0, 0],
+      [0, 0, 0, 'A10', 'A11', 'A12', 'A13', 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 'A0', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A00', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A01', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A02', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A03', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A04', 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
   });
+});
+
+test("10: Can create ships and place them in board's array", () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'horizontal');
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', 'A01', 'A02', 'A03', 'A04'],
+      },
+      { length: 4, shipCode: 'A1', shipHealth: ['A10', 'A11', 'A12', 'A13'] },
+    ],
+  });
+});
+
+test("11: If second ship can't be placed, do not put it in allShips array", () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'vertical');
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', 'A01', 'A02', 'A03', 'A04'],
+      },
+    ],
+  });
+});
+
+test("12: Hit ship using Gameboard's receiveAttack function", () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.receiveAttack(4, 3);
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', -1, 'A02', 'A03', 'A04'],
+      },
+    ],
+    missedShots: [],
+  });
+});
+
+test("13: Hit second ship using Gameboard's receiveAttack function", () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'horizontal');
+  testBoard.receiveAttack(1, 6);
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', 'A01', 'A02', 'A03', 'A04'],
+      },
+      { length: 4, shipCode: 'A1', shipHealth: ['A10', 'A11', 'A12', -1] },
+    ],
+    missedShots: [],
+  });
+});
+
+test('14: Multiple ships on board, miss shot', () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'horizontal');
+  testBoard.receiveAttack(0, 6);
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', 'A01', 'A02', 'A03', 'A04'],
+      },
+      { length: 4, shipCode: 'A1', shipHealth: ['A10', 'A11', 'A12', 'A13'] },
+    ],
+    missedShots: [[0, 6]],
+  });
+});
+
+test('15: Miss shot and see board unchanged', () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'horizontal');
+  testBoard.receiveAttack(0, 6);
+  expect(testBoard).toMatchObject({
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A10', 'A11', 'A12', 'A13', 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A00', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A01', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A02', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A03', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A04', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+});
+
+test('16: Hit ship and see board changed', () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  const testShip2 = Ship(4, 'A1');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.placeShip(testShip2, 1, 3, 'horizontal');
+  testBoard.receiveAttack(1, 6);
+  expect(testBoard).toMatchObject({
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A10', 'A11', 'A12', -1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A00', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A01', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A02', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A03', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 'A04', 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+});
+
+test('17: Miss multiple times', () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.receiveAttack(7, 2);
+  testBoard.receiveAttack(9, 9);
+  testBoard.receiveAttack(2, 3);
+  expect(testBoard).toMatchObject({
+    allShips: [
+      {
+        length: 5,
+        shipCode: 'A0',
+        shipHealth: ['A00', 'A01', 'A02', 'A03', 'A04'],
+      },
+    ],
+    missedShots: [
+      [7, 2],
+      [9, 9],
+      [2, 3],
+    ],
+  });
+});
+
+test('18: Hit ship multiple times and see board changed', () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.receiveAttack(3, 3);
+  testBoard.receiveAttack(4, 3);
+  testBoard.receiveAttack(5, 3);
+  testBoard.receiveAttack(6, 3);
+  testBoard.receiveAttack(7, 3);
+  expect(testBoard).toMatchObject({
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+});
+
+test("19: Sink ship using Gameboard's receiveAttack() function", () => {
+  const testBoard = Gameboard();
+  const testShip1 = Ship(5, 'A0');
+  testBoard.placeShip(testShip1, 3, 3, 'vertical');
+  testBoard.receiveAttack(3, 3);
+  testBoard.receiveAttack(4, 3);
+  testBoard.receiveAttack(5, 3);
+  testBoard.receiveAttack(6, 3);
+  testBoard.receiveAttack(7, 3);
+  expect(testShip1.isSunk()).toBe(true);
 });
