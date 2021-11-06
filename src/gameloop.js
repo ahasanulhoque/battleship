eef1a988d401e6adaf1df2b17753686c104990ee
Ahasanul Hoque <ahoque24@gmail.com>
2021-11-05 20:33:11 -0400

    Initialized AI ships with manual placement

--- src/gameloop.js
+++ src/gameloop.js
@@ -15,7 +15,7 @@ const gameloop = (function loopThroughGame() {
   const humanPlayer = Player('human', aiBoard);
   const aiPlayer = Player('AI', humanBoard);
 
-  // Initialize the ships (***MANUALLY PLACING FOR NOW***)
+  // Initialize the human ships (***MANUALLY PLACING FOR NOW***)
   humanBoard.placeShip(Ship(5, 'A0'), 8, 4, 'horizontal');
   humanBoard.placeShip(Ship(4, 'A1'), 1, 1, 'vertical');
   humanBoard.placeShip(Ship(3, 'A2'), 0, 9, 'vertical');
@@ -24,6 +24,13 @@ const gameloop = (function loopThroughGame() {
 
   // Update human player's board in DOM
   renderBoard(humanBoard.board, document.querySelector('#board-one'));
+
+  // Initialize the AI ships (***MANUALLY PLACING FOR NOW***)
+  aiBoard.placeShip(Ship(5, 'B0'), 0, 2, 'vertical');
+  aiBoard.placeShip(Ship(4, 'B1'), 6, 3, 'horizontal');
+  aiBoard.placeShip(Ship(3, 'B2'), 9, 0, 'horizontal');
+  aiBoard.placeShip(Ship(3, 'B3'), 1, 3, 'vertical');
+  aiBoard.placeShip(Ship(2, 'B4'), 5, 7, 'vertical');
 })();
 
 export { gameloop };
