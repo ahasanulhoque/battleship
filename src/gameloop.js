8b85e3c98dd3b979eae27e3da734db00bd909e40
Ahasanul Hoque <ahoque24@gmail.com>
2021-12-10 14:55:31 -0500

    Added function restartGame(), triggered by event listener. Also moved human attacking ability to new function playGame(), also triggered by event listener.

--- src/gameloop.js
+++ src/gameloop.js
@@ -14,16 +14,19 @@ const gameloop = (function loopThroughGame() {
   createPage(document.querySelector('#content'));
 
   // Initialize the boards
-  const humanBoard = Gameboard();
-  const aiBoard = Gameboard();
+  let humanBoard = Gameboard();
+  let aiBoard = Gameboard();
 
   // Initialize the players
-  const humanPlayer = Player('human', aiBoard.board);
-  const aiPlayer = Player('AI', humanBoard.board);
+  let humanPlayer = Player('human', aiBoard.board);
+  let aiPlayer = Player('AI', humanBoard.board);
 
   // Select the boards in the DOM
-  const boardOne = document.querySelector('#board-one');
-  const boardTwo = document.querySelector('#board-two');
+  let boardOne = document.querySelector('#board-one');
+  let boardTwo = document.querySelector('#board-two');
+
+  // Select the button to restart game after it is over
+  let restartButton = document.querySelector('#play-again-button');
 
   // Initialize the human ships (***MANUALLY PLACING FOR NOW***)
   humanBoard.placeShip(Ship(5, 'A0'), 8, 4, 'horizontal');
@@ -42,8 +45,8 @@ const gameloop = (function loopThroughGame() {
   aiBoard.placeShip(Ship(3, 'B3'), 1, 3, 'vertical');
   aiBoard.placeShip(Ship(2, 'B4'), 5, 7, 'vertical');
 
-  // Attach event listener to enemy board to allow human to attack it
-  boardTwo.onclick = (e) => {
+  // This function runs when the human player clicks the AI board to attack
+  const playGame = function playOnHumanClick(e) {
     // Choose target based on where player clicked
     const humanTarget = humanPlayer.chooseTarget(
       e.target.id.charAt(14),
@@ -84,6 +87,70 @@ const gameloop = (function loopThroughGame() {
         result
       );
     }
+  };
+
+  // This function restarts the game
+  const restartGame = function restartGameOnButtonClick() {
+    // Remove all child elements of #content div
+    while (document.querySelector('#content').firstChild) {
+      document
+        .querySelector('#content')
+        .removeChild(document.querySelector('#content').firstChild);
+    }
+
+    // Render page again
+    createPage(document.querySelector('#content'));
+
+    // Select boards and restart button again
+    boardOne = document.querySelector('#board-one');
+    boardTwo = document.querySelector('#board-two');
+
+    restartButton = document.querySelector('#play-again-button');
+
+    // Initialize the boards again
+    humanBoard = Gameboard();
+    aiBoard = Gameboard();
+
+    // Initialize the players again
+    humanPlayer = Player('human', aiBoard.board);
+    aiPlayer = Player('AI', humanBoard.board);
+
+    // Initialize the human ships (***MANUALLY PLACING FOR NOW***)
+    humanBoard.placeShip(Ship(5, 'A0'), 8, 4, 'horizontal');
+    humanBoard.placeShip(Ship(4, 'A1'), 1, 1, 'vertical');
+    humanBoard.placeShip(Ship(3, 'A2'), 0, 9, 'vertical');
+    humanBoard.placeShip(Ship(3, 'A3'), 3, 3, 'horizontal');
+    humanBoard.placeShip(Ship(2, 'A4'), 5, 7, 'horizontal');
+
+    // Update human player's board in DOM
+    renderBoard(humanBoard.board, boardOne);
+
+    // Initialize the AI ships (***MANUALLY PLACING FOR NOW***)
+    aiBoard.placeShip(Ship(5, 'B0'), 0, 2, 'vertical');
+    aiBoard.placeShip(Ship(4, 'B1'), 6, 3, 'horizontal');
+    aiBoard.placeShip(Ship(3, 'B2'), 9, 0, 'horizontal');
+    aiBoard.placeShip(Ship(3, 'B3'), 1, 3, 'vertical');
+    aiBoard.placeShip(Ship(2, 'B4'), 5, 7, 'vertical');
+
+    // Attach event listener to enemy board to allow human to attack it again
+    boardTwo.onclick = (e) => {
+      playGame(e);
+    };
+
+    // Attach event listener to restart button to allow human to restart again
+    restartButton.onclick = () => {
+      restartGame();
+    };
+  };
+
+  // Attach event listener to enemy board to allow human to attack it
+  boardTwo.onclick = (e) => {
+    playGame(e);
+  };
+
+  // Attach even listener to restart button to allow human to restart
+  restartButton.onclick = () => {
+    restartGame();
   };
 })();
 
